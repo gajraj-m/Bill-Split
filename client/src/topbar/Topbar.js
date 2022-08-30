8642d8150ab00ab1897597e8b45cfe56b0d3b264
Gajraj Mohini <mohinigajraj145@gmail.com>
2022-08-30 10:30:22 +0530

    client added

---
+++ client/src/topbar/Topbar.js
@@ -0,0 +1,14 @@
+import "./styles.css"
+
+export default function Topbar(){
+    <div className="container">
+        <div className="topbarLeft">
+            <div className="logo">
+                
+            </div>
+        </div>
+        <div className="topbarRight">
+
+        </div>
+    </div>
+}
